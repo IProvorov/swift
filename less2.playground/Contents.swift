59c34783017e45f80d430790f91b46e709b6279c
i.provorov <igor.provorov@alseda.by>
2020-01-14 21:20:46 +0300

    lesson 14.01 Type

--- less2.playground/Contents.swift
+++ less2.playground/Contents.swift
@@ -1,4 +1,32 @@
 import UIKit
 
-var str = "Hello, playground"
-///
+//class Play {
+//    func playGround ()  {
+//        integer = 10
+//    }
+//
+//    var integer : Int = 0
+//
+//}
+let integer: Int = 0
+integer
+let maxConst = integer
+
+var varaible: Int = integer
+varaible = 5
+varaible + 1
+
+var varaibleSecond: Int = 5
+
+var intoToDouble = Double(varaible)
+var intoToDoubleSecond = Double(varaibleSecond)
+
+var boolean1: Bool = true;
+
+var string : String = "str"
+
+
+
+
+
+
